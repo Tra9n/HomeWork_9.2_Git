f09c0ae41feb4af4a32b58f25da14d6ac0957654
Григорий Соловьёв <pro100.prometey@mail.ru>
2026-05-05 15:09:49 +0300

    lost fixes

---
+++ src/masks.py
@@ -0,0 +1,14 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция принимает на вход номер карты и возвращает ее маску"""
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+
+
+get_mask_card_number(input())
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функция принимает на вход номер счета и возвращает его маску"""
+    return f"**{account_number[-4:]}"
+
+
+get_mask_account(input())
