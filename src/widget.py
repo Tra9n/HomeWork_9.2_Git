303eaef4b14c944df5758ab4d5b3dfdf0b7c96a6
pro10 <pro100.prometey@mail.ru>
2026-05-05 22:42:36 +0300

    Homework 9.2 готовое дз

--- src/widget.py
+++ src/widget.py
@@ -2,7 +2,10 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(card_namber: str) -> str:
-    """ функция которая умеет обрабатывать информацию как о картах, так и о счетах."""
+    """
+    Функция которая умеет обрабатывать информацию как о картах,
+     так и о счетах.
+    """
     if card_namber[-20:].isdigit():
         return f"Счет {get_mask_account(card_namber[-20:])}"
     else:
@@ -10,11 +13,13 @@ def mask_account_card(card_namber: str) -> str:
 
 
 def get_data(time_card: str) -> str:
-    """принимает системную дату/время, возвращает дату в формате ДД.ММ.ГГГГ"""
+    """
+    Принимает системную дату/время, возвращает
+    дату в формате ДД.ММ.ГГГГ
+    """
     return time_card[8:10] + "." + time_card[5:7] + "." + time_card[:4]
 
 
-if __name__ == '__main__':
-    print(mask_account_card('MasterCard 7158300734726758'))
-    print(mask_account_card('Счет 64686473678894779589'))
-
+if __name__ == "__main__":
+    print(mask_account_card("MasterCard 7158300734726758"))
+    print(mask_account_card("Счет 64686473678894779589"))
